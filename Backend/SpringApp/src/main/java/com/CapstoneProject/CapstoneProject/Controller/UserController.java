3407c316010ff986e72456bd2cfbdac267254bfb
MartinArchila <archilamartin12@gmail.com>
2024-11-10 18:30:35 -0500

    Connected the backend with frontend for the login and signup page

--- Backend/SpringApp/src/main/java/com/CapstoneProject/CapstoneProject/Controller/UserController.java
+++ Backend/SpringApp/src/main/java/com/CapstoneProject/CapstoneProject/Controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin("*")
 
 public class UserController {
 
