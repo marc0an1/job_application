fca5d5dac0470ef224e235eea5a701b501bff000
nicolitoo <nastros3@outlook.com>
2024-11-17 13:54:07 -0500

    Refactor routing to include job applications and calendar components; update menu items accordingly

--- Front-end/sakai-ng-master/src/app/app-routing.module.ts
+++ Front-end/sakai-ng-master/src/app/app-routing.module.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import { RouterModule, Routes } from '@angular/router';
-=======
->>>>>>> d61f5df35c2db92fd49f60feef04664bf019cd7e
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './demo/components/auth/auth.guard'; // Adjust the path as necessary
@@ -12,77 +8,25 @@ import { MyJobApplicationsComponent } from './demo/components/my-job-application
 import { MyCalendarComponent } from './demo/components/my-calendar/my-calendar.component';
 
 // const routes: Routes = [
-//     { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-//     // { path: 'dashboard', component: DashboardComponent},
-//     // { path: 'my-job-applications', component: MyJobApplicationsComponent},
-//     // { path: 'my-calendar', component: MyCalendarComponent},
+//     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 //     {
-//                 path: '', component: AppLayoutComponent,
-//                 children: [
-//                     { path: 'dashboard', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-//                     //{ path: 'my-job-applications', loadChildren: () => import('./demo/components/my-job-applications/my-job-applications.module').then(m => m.MyJobApplicationsModule) },
-//                     { path: 'my-calendar', loadChildren: () => import('./demo/components/my-calendar/my-calendar.module').then(m => m.MyCalendarModule) },
-//                     { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-//                     { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
-//                     { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
-//                     { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
-//                     { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
-//                     { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
-//                 ]
-//             },
-//             { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
-//             { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
-//             { path: 'notfound', component: NotfoundComponent },
-//             { path: '**', redirectTo: '/notfound' },
+//         path: '', component: AppLayoutComponent,
+//         children: [
+//             { path: 'dashboard', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
+//             { path: 'my-job-applications', loadChildren: () => import('./demo/components/my-job-applications/my-job-applications.module').then(m => m.MyJobApplicationsModule) },
+//             { path: 'my-calendar', loadChildren: () => import('./demo/components/my-calendar/my-calendar.module').then(m => m.MyCalendarModule) },
+//             { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
+//             { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
+//             { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
+//             { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
+//             { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
+//         ]
+//     },
+//     { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
+//     { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
+//     { path: 'notfound', component: NotfoundComponent },
+//     { path: '**', redirectTo: '/notfound' }
 // ];
-
-
-// @NgModule({
-//     imports: [
-//         RouterModule.forRoot([
-//             {
-//                 path: '', component: AppLayoutComponent,
-//                 children: [
-//                     { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-//                     { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
-//                     { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
-//                     { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
-//                     { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
-//                     { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
-//                 ]
-//             },
-//             { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
-//             { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
-//             { path: 'notfound', component: NotfoundComponent },
-//             { path: '**', redirectTo: '/notfound' },
-//         ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
-//     ],
-//     exports: [RouterModule]
-// })
-
-// export class AppRoutingModule {
-// }
-
-const routes: Routes = [
-    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    {
-        path: '', component: AppLayoutComponent,
-        children: [
-            { path: 'dashboard', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-            { path: 'my-job-applications', loadChildren: () => import('./demo/components/my-job-applications/my-job-applications.module').then(m => m.MyJobApplicationsModule) },
-            { path: 'my-calendar', loadChildren: () => import('./demo/components/my-calendar/my-calendar.module').then(m => m.MyCalendarModule) },
-            { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
-            { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
-            { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
-            { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
-            { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
-        ]
-    },
-    { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
-    { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
-    { path: 'notfound', component: NotfoundComponent },
-    { path: '**', redirectTo: '/notfound' }
-];
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' }, // Redirect context root to login
@@ -92,6 +36,8 @@ const routes: Routes = [
     path: '', component: AppLayoutComponent,
     children: [
       { path: 'dashboard', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule), canActivate: [AuthGuard] },
+      { path: 'my-job-applications', loadChildren: () => import('./demo/components/my-job-applications/my-job-applications.module').then(m => m.MyJobApplicationsModule) },
+      { path: 'my-calendar', loadChildren: () => import('./demo/components/my-calendar/my-calendar.module').then(m => m.MyCalendarModule) },
       { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule), canActivate: [AuthGuard] },
       { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule), canActivate: [AuthGuard] },
       { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule), canActivate: [AuthGuard] },
@@ -106,14 +52,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-<<<<<<< HEAD
     imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
-=======
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
->>>>>>> d61f5df35c2db92fd49f60feef04664bf019cd7e
